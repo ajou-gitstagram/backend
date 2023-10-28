d02cdbfe84bb4e80bc1bc68adb85fc3b08e3ac40
taerim0 <taerim@ajou.ac.kr>
2023-10-29 03:43:27 +0900

    change post.js

--- router/post.js
+++ router/post.js
@@ -6,30 +6,15 @@ const bodyParser = require('body-parser'); // Import bodyParser
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', (req, res) => {
-    userId = req.query.id
-
-    res.send(`
-    <form method="post" action="/post?id=${userId}">
-      <input type="text" name="content" placeholder="내용"><br>
-      <input type="text" name="tags" placeholder="태그"><br>
-      <input type="submit" value="게시물 생성">
-    </form>
-    `);
-});
-
-// 게시물 생성 POST 라우트
-router.post('/', (req, res) => {
     const jsonFile = fs.readFileSync('./data/post.json', 'utf8');
     const jsonData = JSON.parse(jsonFile);
     const posts = jsonData.post;
 
-    userId = req.query.id
-
-    const content = req.body.content;
-    const tags = req.body.tags.split(',').map(tag => tag.trim())
+    const content = req.query.contents;
+    const tag = JSON.parse(req.query.tags)
 
     // 게시물 데이터를 배열에 추가
-    posts.push({"uid": posts.length, "id" : userId, "content" : content, "like" : 0,  "tag" : tags});
+    posts.push({"uid": posts.length, "id" : userId, "content" : content, "like" : 0,  "tag" : tag});
 
     // Save the updated posts to post.json
     fs.writeFileSync('./data/post.json', JSON.stringify({ "post" : posts }))
@@ -37,6 +22,27 @@ router.post('/', (req, res) => {
 
     res.redirect(`/home?id=${userId}`)
 });
+
+// // 게시물 생성 POST 라우트
+// router.post('/', (req, res) => {
+//     const jsonFile = fs.readFileSync('./data/post.json', 'utf8');
+//     const jsonData = JSON.parse(jsonFile);
+//     const posts = jsonData.post;
+
+//     userId = req.query.id
+
+//     const content = req.body.content;
+//     const tags = req.body.tags.split(',').map(tag => tag.trim())
+
+//     // 게시물 데이터를 배열에 추가
+//     posts.push({"uid": posts.length, "id" : userId, "content" : content, "like" : 0,  "tag" : tags});
+
+//     // Save the updated posts to post.json
+//     fs.writeFileSync('./data/post.json', JSON.stringify({ "post" : posts }))
+//     console.log("게시물 저장 완료")
+
+//     res.redirect(`/home?id=${userId}`)
+// });
 
 
 module.exports = router;
