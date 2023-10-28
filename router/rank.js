0cfc68d4c5a3a6c73891c86984900c6d011b7bca
taerim0 <taerim@ajou.ac.kr>
2023-10-28 19:50:24 +0900

    rankjs change

--- router/rank.js
+++ router/rank.js
@@ -32,45 +32,51 @@ let people =
     }
 ]
 
-console.log(people);
+// console.log(people);
 
 router.get('/:userId', async (req, res) => {
     const userId = req.params.userId;
     const sorttype = req.query.type;
 
-    let cnt = 0;
-
-    try {
-        const response = await axios.get(`${gitlabBaseUrl}/users/${userId}/events`);
-        const activity_log = response.data
-    
-        console.log(activity_log);
-        console.log(activity_log.length);    
-     
-        for (let i = 0; i < activity_log.length; i++){
-            if (activity_log[i].action_name == 'pushed new' || activity_log[i].action_name == 'pushed to') cnt++
-        }
-    
-        console.log(cnt);
-        console.log(sorttype)
-        
-    }   
-    catch(e) {
-            // 오류 발생시 실행
-        
-            console.log(e);
-            res.status(500).json({ error: '오류 발생' });
-    }
     if (sorttype == 'commits')
     {
-        
+        let cnt = 0;
 
-        people.push({ id: userId, commits: cnt })
+        try {
+            const response = await axios.get(`${gitlabBaseUrl}/users/${userId}/events`);
+            const activity_log = response.data
+        
+            // console.log(activity_log);
+            console.log(activity_log.length);    
+         
+            for (let i = 0; i < activity_log.length; i++){
+                if (activity_log[i].action_name == 'pushed new' || activity_log[i].action_name == 'pushed to') cnt++
+            }
+        
+            console.log(cnt);
+            console.log(sorttype)
+            
+        }   
+        catch(e) {
+                // 오류 발생시 실행
+            
+                console.log(e);
+                res.status(500).json({ error: '오류 발생' });
+        }
+    
+        var returnValue = people.find(function(data){ return data.id === userId});
+    
+        if (returnValue === undefined) people.push({ id: userId, commits: cnt })
+
         const temp = people.sort((a,b) => {return b.commits - a.commits});
 
         res.json(temp);
     }
 
+    if (sorttype == 'likes')
+    {
+        
+    }
 })
 
 module.exports = router;
