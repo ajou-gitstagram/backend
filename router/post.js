b8a5d83698a5b9c9ccc8d4e43f1649b242cc509b
taerim0 <taerim@ajou.ac.kr>
2023-10-29 02:42:02 +0900

    add about post activity to posts.json

--- router/post.js
+++ router/post.js
@@ -10,7 +10,6 @@ router.get('/', (req, res) => {
 
     res.send(`
     <form method="post" action="/post?id=${userId}">
-      <input type="text" name="id" placeholder="ID"><br>
       <input type="text" name="content" placeholder="내용"><br>
       <input type="text" name="tags" placeholder="태그"><br>
       <input type="submit" value="게시물 생성">
@@ -26,12 +25,11 @@ router.post('/', (req, res) => {
 
     userId = req.query.id
 
-    const id = req.body.id;
     const content = req.body.content;
     const tags = req.body.tags.split(',').map(tag => tag.trim())
 
     // 게시물 데이터를 배열에 추가
-    posts.push({"uid": posts.length, "id" : id, "content" : content, "like" : 0,  "tag" : tags});
+    posts.push({"uid": posts.length, "id" : userId, "content" : content, "like" : 0,  "tag" : tags});
 
     // Save the updated posts to post.json
     fs.writeFileSync('./data/post.json', JSON.stringify({ "post" : posts }))
